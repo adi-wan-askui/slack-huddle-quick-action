72001fa669dff26d8553f47226350340652b1580
Adrian Stritzinger <Adrian.Stritzinger@askui.com>
2022-08-14 11:31:17 +0200

    add feat of inviting someone to huddle on slack

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const { UiControlClient } = require('askui');
 
+async function inviteToHuddleOnSlack(aui, nameOfCallee) {
+    await aui.pressTwoKeys('command', 'space').exec();
+    await aui.type('Slack').exec();
+    await aui.pressKey('enter').exec();
+    await aui.click().text().containsText('Search').in().header().exec();
+    await aui.click().button().withText('People').below().text().withText('I\'m looking for...').exec();
+    await aui.type(nameOfCallee).exec();
+    await aui.click().text().withText(nameOfCallee).rightOf().image().exec();
+    await aui.click().icon().withText('phone').rightOf().text().withText(nameOfCallee).exec();
+    await aui.click().text().withText('Start a huddle').exec();
+}
+
 async function main() {
     let aui = await UiControlClient.build();
     await aui.connect();
-    await aui.click().text().exec();
+    
+    const nameOfCallee = process.argv[2];
+    await inviteToHuddleOnSlack(aui, nameOfCallee);
+
     aui.close();
 }
 
